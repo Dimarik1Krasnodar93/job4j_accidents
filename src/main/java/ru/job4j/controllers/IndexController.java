4aa97708ed38b2d767b4a296735ffa05b4d576bb
Dimarik <krasnodar93@mail.ru>
2023-03-02 21:05:25 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/controllers/IndexController.java
+++ src/main/java/ru/job4j/controllers/IndexController.java
@@ -1,12 +1,14 @@
 package ru.job4j.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
