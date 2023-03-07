817e3b11ad22b1b2f0af62563f9f93e2863211be
Dimarik <krasnodar93@mail.ru>
2023-03-07 10:36:00 +0300

    RequestParam

--- src/main/java/ru/job4j/controllers/AccidentController.java
+++ src/main/java/ru/job4j/controllers/AccidentController.java
@@ -3,10 +3,7 @@ package ru.job4j.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
 
@@ -32,8 +29,8 @@ public class AccidentController {
         return "createAccident";
     }
 
-    @GetMapping("/formUpdateAccident/{id}")
-    public String formUpdateAccident(Model model, @PathVariable("id") int id) {
+    @GetMapping("/formUpdateAccident")
+    public String formUpdateAccident(Model model, @RequestParam("id") int id) {
         Accident accident = accidentService.getById(id);
         model.addAttribute("accident", accident);
         return "updateAccident";
